9a568b24a5d467295269bd3e4b8ffaf37900fff6
YDidkivskiy <y.didkivskyy_fit_4_23_b_d@knute.edu.ua>
2023-11-24 02:43:23 +0200

    Дідківський Ярослав ФІТ 1-4

---
+++ 4.11.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+    system("chcp 65001");
+    double a1, b1, x;
+    int i = 11;
+    int k = i;
+    int low = k - i;
+    int up = i + 5;
+    int up1 = i + 8;
+
+    std::cout << "Введіть значення a: ";
+    std::cin >> a1;
+
+    std::cout << "Введіть значення b: ";
+    std::cin >> b1;
+
+    double z = a1 - 2 * b1;
+
+
+    x = 8 * log10(fabs(sin(x) + 5));
+
+    double a = 0;
+    double b = 0;
+
+    for (int j = low; j <= up; ++j) {
+        a += x * (j + 5) * k;
+    }
+
+
+    std::cout << "Число i =  " << i << "\nЧисло a = " << a << "\nЧисло z = " << z << std::endl;
+
+    return 0;
+}
